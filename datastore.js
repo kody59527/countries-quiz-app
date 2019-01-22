10f433ed2b50ebd68ebd9e47a74936c79d6ce883
Kody Baldwin <kody59527@gmail.com>
2019-01-22 14:23:07 -0600

    Beginning framework for flag images and alts in datastore

---
+++ datastore.js
@@ -0,0 +1,122 @@
+const STORE = [
+    {
+        question: '' ,
+        answers: [
+        '', 
+        '', 
+        '', 
+        ''
+        ],
+        correctAnswer: '',
+        //flagImg: 'https://i.imgur.com/h9xEeq2.png',
+        //alt: 'The flag of the United Kingdom'
+    },
+    {
+        question: '' ,
+        answers: [
+        '', 
+        '', 
+        '', 
+        ''
+        ],
+        correctAnswer: '',
+        //flagImg: 'https://i.imgur.com/uiA4Bcr.png',
+        //alt: 'The flag of South Korea'
+    },
+    {
+        question: '' ,
+        answers: [
+        '', 
+        '', 
+        '', 
+        ''
+        ],
+        correctAnswer: '',
+        //flagImg: 'https://i.imgur.com/Z1A0p5u.png',
+        //alt: 'The flag of Italy'
+    },
+    {
+        question: '' ,
+        answers: [
+        '', 
+        '', 
+        '', 
+        ''
+        ],
+        correctAnswer: '',
+        //flagImg: 'https://i.imgur.com/utqGIyq.png',
+        //alt: 'The flag of Indonesia'
+    },
+    {
+        question: '' ,
+        answers: [
+        '', 
+        '', 
+        '', 
+        ''
+        ],
+        correctAnswer: '',
+        //flagImg: 'https://i.imgur.com/ENiEaCi.png',
+        //alt: 'The flag of Kenya'
+    },
+    {
+        question: '' ,
+        answers: [
+        '', 
+        '', 
+        '', 
+        ''
+        ],
+        correctAnswer: '',
+        //flagImg: 'https://i.imgur.com/tdZemW2.png',
+        //alt: 'The flag of Czechoslovakia'
+    },
+    {
+        question: '' ,
+        answers: [
+        '', 
+        '', 
+        '', 
+        ''
+        ],
+        correctAnswer: '',
+        //flagImg: 'https://i.imgur.com/GME7niX.png',
+        //alt: 'The flag of Singapore'
+    },
+    {
+        question: '' ,
+        answers: [
+        '', 
+        '', 
+        '', 
+        ''
+        ],
+        correctAnswer: '',
+        //flagImg: 'https://i.imgur.com/Xh5OgE6.png',
+        //alt: 'The flag of Thailand'
+    },
+    {
+        question: '' ,
+        answers: [
+        '', 
+        '', 
+        '', 
+        ''
+        ],
+        correctAnswer: '',
+        //flagImg: 'https://i.imgur.com/Wt8L8HY.png',
+        //alt: 'The flag of Papua New Guinea'
+    },
+    {
+        question: '' ,
+        answers: [
+        '', 
+        '', 
+        '', 
+        ''
+        ],
+        correctAnswer: '',
+        //flagImg: 'https://i.imgur.com/3Cz8act.png',
+        //alt: 'The flag of Sweden'
+    },
+];
